5ea0d9b3723afd9b0ada102b79574cd81a594f1a
Amitai Salomon <amitaisal@gmail.com>
2022-03-06 13:06:16 +0200

    first try

---
+++ search.py
@@ -0,0 +1,22 @@
+# search
+import state
+import frontier
+
+
+def search(n):
+    s = state.create(n)
+    print(s)
+    f = frontier.create(s)
+    print(state.hdistance(s))
+
+    while not frontier.is_empty(f):
+        s = frontier.remove(f)
+        if state.is_target(s):
+            return s
+        ns = state.get_next(s)
+        for i in ns:
+            frontier.insert(f, i)
+    return 0
+
+
+print(search(3))
---
+++ state.py
@@ -0,0 +1,81 @@
+'''
+The state is a list of 2 items: the board, the path
+The target is :
+012
+345
+678
+
+'''
+import random
+import math
+
+
+def get_next(x):
+    ns = []
+    for i in "<>v^":
+        s = x[0][:]
+        if_legal(s, i)
+        if s.index(0) != x[0].index(0) and \
+                (x[1] == "" or x[1][-1] != "><^v"["<>v^".index(i)]):
+            ns.append([s, x[1] + i])
+    return ns
+
+
+# returns a random board nXn
+def create(n):
+    s = list(range(n * n))
+    m = "<>v^"
+    for i in range(n ** 3):
+        if_legal(s, m[random.randrange(4)])
+    return [s, ""]
+
+
+def path_len(x):
+    return len(x[1])
+
+
+def is_target(x):
+    n = len(x[0])
+    return x[0] == list(range(n))
+
+
+def hdistance(s):
+    n = math.sqrt(len(s[0]))
+    sum = 0
+    for i in len(s[0]):
+        y = i % n
+        x = s[0][i] % n
+
+        z = 0
+        w = 0
+        for j in n:
+            if j * n <= i < (j + 1) * n:
+                z = j
+
+        for j in n:
+            if j * n <= s[0][i] < (j + 1) * n:
+                w = j
+
+        col = abs(y - x)
+        row = abs(w - z)
+        sum += max(row, col)
+
+    return sum
+
+
+#############################
+def if_legal(x, m):
+    n = int(math.sqrt(len(x)))
+    z = x.index(0)
+    if z % n > 0 and m == "<":
+        x[z] = x[z - 1]
+        x[z - 1] = 0
+    elif z % n < n - 1 and m == ">":
+        x[z] = x[z + 1]
+        x[z + 1] = 0
+    elif z >= n and m == "^":
+        x[z] = x[z - n]
+        x[z - n] = 0
+    elif z < n * n - n and m == "v":
+        x[z] = x[z + n]
+        x[z + n] = 0
